196b46197ae75e6cb40d82781d5162fd41cc85bf
3기-1반 애범 <gogot1103@naver.com>
2022-02-07 17:03:21 +0900

    TodoList 완성

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,30 +1,95 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../models/models';
-import { XCircleIcon, PencilIcon, CheckCircleIcon } from '@heroicons/react/solid';
+import {
+  XCircleIcon,
+  PencilIcon,
+  CheckCircleIcon,
+} from '@heroicons/react/solid';
 
 type Props = {
-    todo: Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-    return (
-        <form className="flex w-1/3	rounded-md p-[20px] mt-[15px] mx-1 bg-green-600">
-            <span className="flex-1 p-[5px] text-[20px]  focus:outline-none">{todo.todo}</span>
-            <div>
-                <span className="ml-[10px] text-[25px] cursor-pointer">
-                    <XCircleIcon />
-                </span>
-                <span className="ml-[10px] text-[25px] cursor-pointer">
-                    <PencilIcon />
-                </span>
-                <span className="ml-[10px] text-[25px] cursor-pointer">
-                    <CheckCircleIcon />
-                </span>
-            </div>
-        </form>
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
     );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map(todo => (todo.id === id ? { ...todo, todo: editTodo } : todo)),
+    );
+    setEdit(false);
+  };
+
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  return (
+    <form
+      className="flex w-1/3	rounded-md p-[20px] mt-[15px] mx-1 bg-green-600 md:w-full"
+      onSubmit={e => handleEdit(e, todo.id)}
+    >
+      {edit ? (
+        <input
+          ref={inputRef}
+          type="text"
+          value={editTodo}
+          className="flex-1 p-[5px] text-[20px]  focus:outline-none"
+          onChange={e => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
+        <s className="flex-1 p-[5px] text-[20px]  focus:outline-none">
+          {todo.todo}
+        </s>
+      ) : (
+        <span className="flex-1 p-[5px] text-[20px]  focus:outline-none">
+          {todo.todo}
+        </span>
+      )}
+
+      <div>
+        <span
+          className="ml-[10px] text-[25px] cursor-pointer"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <PencilIcon />
+        </span>
+        <span
+          className="ml-[10px] text-[25px] cursor-pointer"
+          onClick={() => handleDelete(todo.id)}
+        >
+          <XCircleIcon />
+        </span>
+        <span
+          className="ml-[10px] text-[25px] cursor-pointer"
+          onClick={() => handleDone(todo.id)}
+        >
+          <CheckCircleIcon />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,18 +3,23 @@ import { Todo } from '../models/models';
 import SingleTodo from './SingleTodo';
 
 interface Props {
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
-    return (
-        <div className="flex flex-wrap w-11/12 justify-evenly">
-            {todos.map(todo => (
-                <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap w-11/12 justify-evenly md:flex-col md:w-11/12">
+      {todos.map(todo => (
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
